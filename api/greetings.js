745d5c9b2a9e9a592dd431ee050e911cd16cd191
HARSHITA PRIYA <harshitapriyaph5@gmail.com>
2023-06-28 23:47:14 +0530

    more changes

--- api/greetings.js
+++ api/greetings.js
@@ -5,4 +5,17 @@ router.get('/hello', (req, res) => {
     res.send('Hello World!');
 });
 
+router.post("/hi", (req,res)=>{
+    const name = req.body.name;
+    try {
+        if(name=="" || name == undefined) throw new Error("name is required");
+        else if(name=="Harshita") res.send("Hi Harshita!");
+        else res.send(`hello ${name}!`);
+    }
+    catch(e){
+        res.send(e.message);
+    }
+    res.send(`Hi ${name}!`);
+});
+
 module.exports = router;
